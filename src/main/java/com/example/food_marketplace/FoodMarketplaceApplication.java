bbcee2e7843a5e28ea28c3f29a49884952e5af08
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2024-09-04 16:54:15 -0300

    First commit

---
+++ src/main/java/com/example/food_marketplace/FoodMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.food_marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodMarketplaceApplication.class, args);
+	}
+
+}
