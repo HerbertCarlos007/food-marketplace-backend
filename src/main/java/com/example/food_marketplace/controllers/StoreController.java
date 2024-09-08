7b61711977544524029be24f8eec9b75092182bb
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2024-09-08 01:28:17 -0300

    feat: Função de buscar loja pelo subdomain

--- src/main/java/com/example/food_marketplace/controllers/StoreController.java
+++ src/main/java/com/example/food_marketplace/controllers/StoreController.java
@@ -1,6 +1,7 @@
 package com.example.food_marketplace.controllers;
 
 import com.example.food_marketplace.domain.store.Store;
+import com.example.food_marketplace.domain.user.User;
 import com.example.food_marketplace.dto.store.StoreRequestDTO;
 import com.example.food_marketplace.service.StoreService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,5 +21,11 @@ public class StoreController {
     public ResponseEntity<Store> createStore(@RequestBody StoreRequestDTO data) {
         Store stores = storeService.createStore(data);
         return ResponseEntity.ok(stores);
+    }
+
+    @GetMapping("/{subdomain}")
+    public ResponseEntity<Store> getBySubdomain(@PathVariable String subdomain) {
+        Store store = storeService.getBySubdomain(subdomain);
+        return ResponseEntity.ok(store);
     }
 }
--- src/main/java/com/example/food_marketplace/service/StoreService.java
+++ src/main/java/com/example/food_marketplace/service/StoreService.java
@@ -4,6 +4,7 @@ import com.example.food_marketplace.domain.store.Store;
 import com.example.food_marketplace.dto.store.StoreRequestDTO;
 import com.example.food_marketplace.repositories.StoreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -24,5 +25,10 @@ public class StoreService {
         Store stores = new Store();
         stores.setSubdomain(data.subdomain());
         return storeRepository.save(stores);
+    }
+
+    public Store getBySubdomain(String subdomain) {
+        return storeRepository.findBySubdomain(subdomain)
+                .orElseThrow(() -> new RuntimeException("Store not found"));
     }
 }
