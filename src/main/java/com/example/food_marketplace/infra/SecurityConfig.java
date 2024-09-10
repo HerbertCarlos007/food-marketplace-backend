eedb374eb6aad1366269ddd0a6a283f8150caf41
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2024-09-10 02:45:23 -0300

    feat: Adicionando rota no securityConfig

--- src/main/java/com/example/food_marketplace/infra/SecurityConfig.java
+++ src/main/java/com/example/food_marketplace/infra/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/store").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/store/{subdomain}").permitAll()
                         .anyRequest().authenticated()
                 )
