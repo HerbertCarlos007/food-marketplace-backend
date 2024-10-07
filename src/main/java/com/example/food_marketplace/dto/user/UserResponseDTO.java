6024dac20c825f9fd7d19f47c3463b290e8fea92
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2024-10-07 14:14:08 -0300

    fix: Corrigindo ordem do DTO

--- src/main/java/com/example/food_marketplace/dto/user/UserResponseDTO.java
+++ src/main/java/com/example/food_marketplace/dto/user/UserResponseDTO.java
@@ -2,5 +2,5 @@ package com.example.food_marketplace.dto.user;
 
 import java.util.UUID;
 
-public record UserResponseDTO(UUID id, String email, String name) {
+public record UserResponseDTO(UUID id, String name, String email) {
 }
