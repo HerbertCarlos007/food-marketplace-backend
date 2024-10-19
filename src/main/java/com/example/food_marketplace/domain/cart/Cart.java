b25c8f054357a782f391419d623bc37db6d0ca00
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2024-10-18 23:38:53 -0300

    feat: Criando arquivos do carrinho de compra

---
+++ src/main/java/com/example/food_marketplace/domain/cart/Cart.java
@@ -0,0 +1,4 @@
+package com.example.food_marketplace.domain.cart;
+
+public class Cart {
+}
---
+++ src/main/java/com/example/food_marketplace/repositories/CartRepository.java
@@ -0,0 +1,4 @@
+package com.example.food_marketplace.repositories;
+
+public interface CartRepository {
+}
