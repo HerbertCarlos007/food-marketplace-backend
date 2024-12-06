a28ad6bf69be6422e41f6d69090c7665fc223678
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2024-12-06 16:37:19 -0300

    feat: Criando custom field dto

---
+++ src/main/java/com/example/food_marketplace/dto/customField/CustomFieldRequestDTO.java
@@ -0,0 +1,4 @@
+package com.example.food_marketplace.dto.customField;
+
+public record CustomFieldRequestDTO(String name, String primary_color, String secondary_color, String logoUrl, String font_name) {
+}
