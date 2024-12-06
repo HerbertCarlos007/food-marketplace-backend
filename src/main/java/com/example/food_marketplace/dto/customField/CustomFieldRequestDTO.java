eef9bf6fd9288204e803ab24cbbd7812f469e5ce
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2024-12-06 17:07:40 -0300

    feat: Adicionando campo storeId

--- src/main/java/com/example/food_marketplace/dto/customField/CustomFieldRequestDTO.java
+++ src/main/java/com/example/food_marketplace/dto/customField/CustomFieldRequestDTO.java
@@ -2,5 +2,7 @@ package com.example.food_marketplace.dto.customField;
 
 import org.springframework.web.multipart.MultipartFile;
 
-public record CustomFieldRequestDTO(String name, String primary_color, String secondary_color, MultipartFile logoUrl, String font_name) {
+import java.util.UUID;
+
+public record CustomFieldRequestDTO(String name, String primary_color, String secondary_color, MultipartFile logoUrl, String font_name, UUID storeId) {
 }
