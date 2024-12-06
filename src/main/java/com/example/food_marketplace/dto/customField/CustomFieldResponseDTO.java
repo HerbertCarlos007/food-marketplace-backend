002948589355adf38ba2af134d839d166d5549ca
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2024-12-06 18:49:33 -0300

    feat: Criando custom field response dto

---
+++ src/main/java/com/example/food_marketplace/dto/customField/CustomFieldResponseDTO.java
@@ -0,0 +1,8 @@
+package com.example.food_marketplace.dto.customField;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
+
+public record CustomFieldResponseDTO(String name, String primary_color, String secondary_color, MultipartFile logoUrl, String font_name, UUID storeId) {
+}
